0c21437a23ea6c3f37d03e5d7db3ddd7b1113947
Amit Dhakad <amit.dhakad@onx.ai>
2021-02-23 16:46:46 +0530

    added appbar, toolbar in header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,8 +1,33 @@
+import { Button } from '@material-ui/core';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
 import React, { Component } from 'react';
-
+import { Link } from 'react-router-dom';
 class Header extends Component {
   render() {
-    return <div>header</div>;
+    return (
+      <AppBar
+        position="fixed"
+        style={{
+          backgroundColor: '#98c5e9',
+          boxShadow: 'none',
+          padding: '10px ',
+          borderBottom: '2px solid #00285e ',
+        }}
+      >
+        <Toolbar styles={{ display: 'flex' }}>
+          <div styles={{ flexGrow: 1 }}>
+            <div className="header_logo">Logo</div>
+          </div>
+          <Link to="/the_team">
+            <Button color="inherit">The team</Button>
+          </Link>
+          <Link to="/the_matches">
+            <Button color="inherit">The matches</Button>
+          </Link>
+        </Toolbar>
+      </AppBar>
+    );
   }
 }
 
