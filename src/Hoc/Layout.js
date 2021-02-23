70135653a9fe3e0334451338316213b642e3ebb4
Amit Dhakad <amit.dhakad@onx.ai>
2021-02-23 15:39:05 +0530

    added header file

--- src/Hoc/Layout.js
+++ src/Hoc/Layout.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Header from '../Components/Header/Header';
 
 const Layout = (props) => {
   return (
     <div>
-      header
+      <Header></Header>
       {props.children}
       footer
     </div>
