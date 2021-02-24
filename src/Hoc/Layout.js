283b11072a34960c7c13f801c1b3194394b0a6b4
Amit Dhakad <amit.dhakad@onx.ai>
2021-02-24 16:13:36 +0530

    added home, featured components

--- src/Hoc/Layout.js
+++ src/Hoc/Layout.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import Footer from '../Components/footer/Footer';
 import Header from '../Components/Header/Header';
+import Home from '../Components/home';
 
 const Layout = (props) => {
   return (
     <div>
       <Header></Header>
-      {props.children}
+      <Switch>
+        <Route exact component={Home} path="/" />
+      </Switch>
       <Footer></Footer>
     </div>
   );
