254d61b0973a5fd414d495375b1d4ee7cf8e781b
amit-dhakad <amit0502dhakad@gmail.com>
2021-03-12 00:11:57 +0530

    added meet the player component

---
+++ src/Components/home/meetPlayers/index.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react';
+import Stripes from '../../../Resources/images/stripes.png';
+import { Tag } from '../../ui/misc';
+export default class MeetPlayers extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div
+        className="home_meetplayers"
+        style={{ background: `#ffffff url(${Stripes})` }}
+      >
+        <div className="container">
+          <div className="home_meetplayers_wrapper">
+            <div className="home_card_wrapper">card</div>
+            <div className="home_text_wrapper">
+              <div>
+                <Tag
+                  bck="#0e1731"
+                  size="100px"
+                  color="#ffffff"
+                  add={{
+                    display: 'inline-block',
+                    marginBottom: '20px',
+                  }}
+                >
+                  Meet
+                </Tag>
+              </div>
+              <div>
+                <Tag
+                  bck="#0e1731"
+                  size="100px"
+                  color="#ffffff"
+                  add={{
+                    display: 'inline-block',
+                    marginBottom: '20px',
+                  }}
+                >
+                  The
+                </Tag>
+              </div>
+              <div>
+                <Tag
+                  bck="#0e1731"
+                  size="100px"
+                  color="#ffffff"
+                  add={{
+                    display: 'inline-block',
+                    marginBottom: '20px',
+                  }}
+                >
+                  Players
+                </Tag>
+              </div>
+              <div>
+                <Tag
+                  bck="#ffffff"
+                  size="27px"
+                  color="#0e1731"
+                  link={true}
+                  linkto="/the_team"
+                  add={{
+                    display: 'inline-block',
+                    marginBottom: '27px',
+                    border: '1px solid #0e1731',
+                  }}
+                >
+                  Meet the here
+                </Tag>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
