bce70f13c8ba4f7752f1561ab76f4f2fa72239bc
amit-dhakad <amit0502dhakad@gmail.com>
2021-03-15 01:11:50 +0530

    completed private and public routes

--- src/index.js
+++ src/index.js
@@ -1,22 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { firebase } from './firebase';
 import reportWebVitals from './reportWebVitals';
 import './Resources/css/app.css';
 import Routes from './routes';
-const App = () => {
+
+const App = (props) => {
   return (
     <BrowserRouter>
-      <Routes />
+      <Routes {...props} />
     </BrowserRouter>
   );
 };
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+
+firebase.auth().onAuthStateChanged((user) => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App user={user} />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+});
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
