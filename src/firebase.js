22dda325b21dcf37a95bc769f5421d5429803d01
amit-dhakad <amit0502dhakad@gmail.com>
2021-03-15 00:07:57 +0530

    added dashboard and sign in completed

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,12 @@
 import 'firebase/app';
 import firebase from 'firebase/app';
+import 'firebase/auth';
 import 'firebase/database';
-
 const firebaseConfig = {};
 
 firebase.initializeApp(firebaseConfig);
 const firebaseDB = firebase.database();
 const firebaseMatches = firebaseDB.ref('matches');
+const firebasePromotions = firebaseDB.ref('promotions');
 
-export { firebase, firebaseMatches };
+export { firebase, firebaseMatches, firebasePromotions };
